60038eb8820ab493e80a2f727a0163bf9653bfc6
ninpnin <vainoyrjanainen@icloud.com>
2022-10-24 13:30:22 +0200

    feat: Sparv annotation script

---
+++ Data/ParlaMint-SE/annotate.fish
@@ -0,0 +1,12 @@
+for f in ParlaMint-SE_*.xml;
+    set ending (string sub --start=-7 $f);
+    if [ "ana.xml" != $ending ]
+        cp $f source
+    end
+end;
+
+conda_init && conda activate data
+rm export/xml_export.pretty/*.xml
+sparv run
+rm source/*.xml
+python3 clean_annotations.py
