be7c033269e56730cd225b3187a8cef6c7743e99
ninpnin <vainoyrjanainen@icloud.com>
2022-11-03 11:49:51 +0200

    feat: track unpopulated versions of the corpus files; remove empty files

--- Scripts/populate_links.py
+++ Scripts/populate_links.py
@@ -2,6 +2,7 @@ from lxml import etree
 import argparse
 from pathlib import Path
 import datetime
+import progressbar
 
 tei_ns = "{http://www.tei-c.org/ns/1.0}"
 xml_ns = "{http://www.w3.org/XML/1998/namespace}"
@@ -10,7 +11,30 @@ parser = etree.XMLParser(remove_blank_text=True)
 
 def find_links(root, args):
     folder = Path(args.path).parent
+    root_id = root.attrib[f"{xml_ns}id"]   
+    corpus_is_annotated = ".ana" in root_id
+    
+    print("Remove old links...")
     for xi in root.findall(f".//{xi_ns}include"):
+        if "ParlaMint-SE_" in xi.attrib.get("href", ""):
+            parent = xi.getparent()
+            parent.remove(xi)
+    
+    print("Add links...")
+    for p in folder.glob("ParlaMint-SE_*.xml"):
+        file_is_annotated = ".ana" in p.suffixes
+        if corpus_is_annotated == file_is_annotated:
+            nsmap = {"xi": xi_ns.replace("{", "").replace("}", "")}
+            include = etree.SubElement(root, f"{xi_ns}include", nsmap=nsmap)
+            include.attrib["href"] = f"{p.stem}{p.suffix}"
+
+    print("Write to original file with updated links...")
+    root_bytes = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
+    with Path(args.path).open("wb") as f:
+        f.write(root_bytes)
+
+    print("Populate includes from individual protocol files...")
+    for xi in progressbar.progressbar(list(root.findall(f".//{xi_ns}include"))):
         ref = xi.attrib["href"]
         with (folder / ref).open() as f:
             linked_root = etree.parse(f, parser).getroot()
@@ -20,26 +44,18 @@ def find_links(root, args):
             print(ref, linked_root)
             xi_parent.insert(xi_parent.index(xi) + 1, linked_root)
         xi_parent.remove(xi)
-    root_id = root.attrib[f"{xml_ns}id"]
     teiHeaders = root.findall(f".//{tei_ns}teiHeader")
     assert len(teiHeaders) == 1
     teiHeader = teiHeaders[0]
 
-    corpus_is_annotated = ".ana" in root_id
-    for p in folder.glob("ParlaMint-SE_*.xml"):
-        file_is_annotated = ".ana" in p.suffixes
-        if corpus_is_annotated == file_is_annotated:
-            nsmap = {"xi": xi_ns.replace("{", "").replace("}", "")}
-            include = etree.SubElement(root, f"{xi_ns}include", nsmap=nsmap)
-            include.attrib["href"] = f"{p.stem}{p.suffix}"
     return root
 
 def main(args):
     parser = etree.XMLParser(remove_blank_text=True)
     root = etree.parse(args.path, parser).getroot()
-    print(root)
     root = find_links(root, args)
 
+    print("Write to outfile...")
     root_bytes = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
     with Path(args.outpath).open("wb") as f:
         f.write(root_bytes)
